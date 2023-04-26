d19e4daa7bff5746f62ee056e90e18ac7270117e
sarwar <sarwar.cse.diu@gmail.com>
2023-04-26 17:09:13 +0600

    add js file with api

--- WeatherApp/Index.js
+++ WeatherApp/Index.js
@@ -1,48 +1,58 @@
+const apikey = "eaed64d65b83117aec0c1a525cadf592";
 
-const apikey="eaed64d65b83117aec0c1a525cadf592";
-const weatherDtEl=document.getElementById("weather-data")
-const cityInp=document.getElementById("city-input")
-const formEl=document.querySelector("form")
+const weatherDataEl = document.getElementById("weather-data");
 
-formEl.addEventListener("submit",(event)=>{
-    event.preventDefault();
-    const cityval=cityInp.value;
-   // console.log(cityval);
-    getWeatherData(cityval);
+const cityInputEl = document.getElementById("city-input");
+
+const formEl = document.querySelector("form");
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const cityValue = cityInputEl.value;
+  getWeatherData(cityValue);
 });
 
-async function getWeatherData(cityval){
-  try{
-    const response=await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${cityval}&appid=${apikey}&units=metric`
+async function getWeatherData(cityValue) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`
     );
-    if(!response.ok){
-        throw new Error("Network response is not ok");
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
-   const data=await response.json();
-  // console.log(data);
-   const temperature=Math.round(data.main.temp);
-   const description=data.weather[0].description
-   const icon=data.weather[0].icon
-   const details=[
-    `Feels like: ${Math.round(data.main.feels_like)}`,
+
+    const data = await response.json();
+
+    const temperature = Math.round(data.main.temp);
+
+    const description = data.weather[0].description;
+
+    const icon = data.weather[0].icon;
+
+    const details = [
+      `Feels like: ${Math.round(data.main.feels_like)}`,
       `Humidity: ${data.main.humidity}%`,
       `Wind speed: ${data.wind.speed} m/s`,
-   ];
-   weatherDataEl.querySelector(
-    ".icon"
-  ).innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">`;
-  
-  weatherDataEl.querySelector(
-    ".temperature"
-  ).textContent = `${temperature}°C`;
+    ];
 
-  weatherDataEl.querySelector(".description").textContent = description;
-  
-}
+    weatherDataEl.querySelector(
+      ".icon"
+    ).innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">`;
+    weatherDataEl.querySelector(
+      ".temperature"
+    ).textContent = `${temperature}°C`;
+    weatherDataEl.querySelector(".description").textContent = description;
 
+    weatherDataEl.querySelector(".details").innerHTML = details
+      .map((detail) => `<div>${detail}</div>`)
+      .join("");
+  } catch (error) {
+    weatherDataEl.querySelector(".icon").innerHTML = "";
+    weatherDataEl.querySelector(".temperature").textContent = "";
+    weatherDataEl.querySelector(".description").textContent =
+      "An error happened, please try again later";
 
-  catch(error){
-   
-  }  
+    weatherDataEl.querySelector(".details").innerHTML = "";
+  }
 }
